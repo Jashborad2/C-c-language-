ec4d1a71507a35b296e31f3e303957dd37d8368d
Jashborad2 <134933006+Jashborad2@users.noreply.github.com>
2023-06-01 20:02:53 +0530

    Add files via upload

--- task1.c
+++ task1.c
@@ -1,10 +1,35 @@
-#include <stdio.h>
-
+#include<stdio.h>
 void main()
 {
-	printf(" \t\t*\t\t*\n");
-	printf("\t *\t\t*\n");
-	printf("*\t\t*\n");
-	printf(" \t*\t\t*\n");
-	printf(" \t\t*\t\t *");
+	int a,b,c;
+	printf("enter a:");
+	scanf("%d",&a);
+	printf("enter b:");
+	scanf("%d",&b);
+    printf("enter c:");
+	scanf("%d",&c);
+	
+	
+	if(a<b)
+	{
+		if(a<c)
+		{
+		    printf("a is min");
+		}
+		else
+		{
+			printf("c is min");
+			
+		}
+	}
+	else{
+		if(b<c)
+		{ 
+		printf("b is min");
+		}
+		else
+		{
+			printf("c is min");
+		}
+	}
 }
