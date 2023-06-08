421b2a3d60f474f855006f2d4b957cb136e15597
Jashborad2 <134933006+Jashborad2@users.noreply.github.com>
2023-06-08 12:49:46 +0530

    Add files via upload

---
+++ day 12/task8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main(){
+	int a,sum,i=0;
+	
+	 printf("enter value :");
+	 scanf("%d",&a);
+	 
+	 while(i<=a){
+	 	sum = sum + i;
+	 	i++;
+	 	
+	 }
+	printf("%d",sum);
+}
